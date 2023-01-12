bafb459215a4a0364d9e6e3048607c72f54cc7a9
zisling <hedi120@gmail.com>
2023-01-12 15:29:12 +0200

    For the students

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# STOMP TEST
+This is a test for bgu spl stomp server
+
+To run this code you need to install the following:
+1. Linux or The VM or The given docker.
+2. python 3.6 or above. you m
+3. install stomp.py
+
+## How to run
+1. Run you server on port 7777
+2. Then run the fallowing command in the terminal:
+   
+ 
+    $ python3 stompTest.py
+
+3. You should see the following output:
+   
+
+    connection number 2 received an error
+    <THE ERROR FRAME YOU SENT>
+    I must not fear.
+    Fear is the mind-killer.
+    Fear is the little-death that brings total obliteration.
+    I will face my fear.
+    I will permit it to pass over me and through me.
+    And when it has gone past I will turn the inner eye to see its path.
+    Where the fear has gone there will be nothing.
+    Only I will remain.
+    --- Frank Herbert, Dune
+
+### How To install stomp.py
+
+    pip3 install stomp.py
+
--- stompTest.py
+++ stompTest.py
@@ -1,77 +1,77 @@
-import os
 import time
 import stomp
+import argparse
 
-def connect_and_subscribe(conn,name,password,Topic):
-    global x
-    x = 1
-    conn.connect(name, password, wait=False)
-    conn.subscribe(destination=Topic, id=x)
-    x += 1
+
+def connect_and_subscribe(conn, name, password, topic):
+    conn.connect(name, password, wait=True)
+    conn.subscribe(destination=topic, id=1)
 
 
 class MyListener(stomp.ConnectionListener):
-    def __init__(self, conn):
+    def __init__(self, conn, id_num):
         self.conn = conn
+        self.id = id_num
 
-    def on_error(self, headers, message):
-        print('received an error "%s"' % headers)
-        print('body: "%s"' % message)
+    def on_error(self, frame):
+        print('connection number %d received an error\n%s' % (self.id, frame))
 
-
-    def on_message(self, headers, message):
-        print('received a message "%s"' % message)
-        for y in range(1):
-            # print(y)
-            time.sleep(1)
-        print('processed message')
+    def on_message(self, frame):
+        if self.id == 1:
+            print('%s' % frame.body)
 
     def on_disconnected(self):
         print('disconnected')
 
-conn1 = stomp.Connection12([('localhost', 7777)])
-conn2 = stomp.Connection12([('localhost', 7777)])
-conn3 = stomp.Connection12([('localhost', 7777)])
-conn4 = stomp.Connection12([('localhost', 7777)])
-conn1.set_listener('', MyListener(conn1))
-conn2.set_listener('', MyListener(conn2))
-time.sleep(2)
-# conn3.set_listener('', MyListener(conn3))
-# conn4.set_listener('', MyListener(conn4))
-connect_and_subscribe(conn1,'zisling','password','/wicther')
-time.sleep(0.1)
-connect_and_subscribe(conn2,'zisling','password','/wicther')
-connect_and_subscribe(conn3,'hedi','password','/wicther')
-connect_and_subscribe(conn4,'Geralt','password','/wicther')
-conn1.send('/wicther',"When a humble bard Graced a ride along With Geralt of Rivia Along came this song")
-time.sleep(0.5)
-if conn2.is_connected():
-    conn2.send('/wicther',"From when the White Wolf fought A silver-tongued devil His army of elves At his hooves did they revel")
-time.sleep(0.5)
-conn3.send('/wicther',"They came after me With masterful deceit Broke down my lute And they kicked in my teeth")
-time.sleep(0.5)
-conn4.send('/wicther','While the devil’s horns Minced our tender meat And so cried the Witcher He can’t be bleat')
-time.sleep(0.5)
-conn1.send('/wicther','Toss a coin to your Witcher O Valley of Plenty O Valley of Plenty, ohToss a coin to Your Witcher O Valley of Plenty')
-time.sleep(0.5)
-if conn2.is_connected():
-    conn2.send('/wicther','At the edge of the world Fight the mighty horde That bashes and breaks you And brings you to mourn')
-time.sleep(0.5)
-conn3.send('/wicther','He thrust every elf Far back on the shelf High up on the mountain From whence it came')
-time.sleep(0.5)
-conn4.send('/wicther','He wiped out your pest Got kicked in his chest He’s a friend of humanity So give him the rest')
-time.sleep(0.5)
-conn1.send('/wicther','That’s my epic tale: A champion prevailed Defeated the villain Now pour him some ale')
-time.sleep(0.5)
-if conn2.is_connected():
-    conn2.send('/wicther',"Toss a coin to your Witcher O' Valley of Plenty O' Valley of Plenty, oh Toss a coin to your Witcher A friend of humanity")
-time.sleep(0.5)
-conn3.send('/wicther',"Toss a coin to your Witcher O' Valley of Plenty O' Valley of Plenty, oh Toss a coin to your Witcher A friend of humanity")
-time.sleep(0.5)
-conn4.send('/wicther',"Toss a coin to your Witcher O' Valley of Plenty O' Valley of Plenty, oh Toss a coin to your Witcher A friend of humanity")
-time.sleep(10)
-conn1.disconnect()
-if conn2.is_connected():
-    conn2.disconnect()
-conn3.disconnect()
-conn4.disconnect()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='STOMP client')
+    parser.add_argument('-ip', '--ip', type=str, default='localhost', help='IP address of the server')
+    parser.add_argument('-p', '--port', type=int, default=7777, help='Port of the server')
+    return parser.parse_args()
+
+
+dune_quote = '''I must not fear.
+Fear is the mind-killer.
+Fear is the little-death that brings total obliteration.
+I will face my fear.
+I will permit it to pass over me and through me.
+And when it has gone past I will turn the inner eye to see its path.
+Where the fear has gone there will be nothing.
+Only I will remain.
+--- Frank Herbert, Dune'''
+con2_message = 'conn 2 should not be connected and it send a message'
+if __name__ == '__main__':
+    args = parse_args()
+    ip_port = [(args.ip, args.port)]
+    conn1 = stomp.Connection12(ip_port)
+    conn2 = stomp.Connection12(ip_port)
+    conn3 = stomp.Connection12(ip_port)
+    conn4 = stomp.Connection12(ip_port)
+    active_cons = [conn1, conn3, conn4]
+    conn1.set_listener('', MyListener(conn1, 1))
+    conn2.set_listener('', MyListener(conn2, 2))
+    conn2.set_listener('', MyListener(conn2, 3))
+    conn2.set_listener('', MyListener(conn2, 4))
+    time.sleep(1)
+    connect_and_subscribe(conn1, 'Paul', 'Atreides', '/dune')
+    connect_and_subscribe(conn2, 'Paul', 'Atreides', '/dune')
+    connect_and_subscribe(conn3, 'Alia', 'OfTheKnife', '/dune')
+    connect_and_subscribe(conn4, 'Lady', 'Jessica', '/dune')
+    time.sleep(1)
+    dune_quote_split = dune_quote.split('\n')
+
+    if conn2.is_connected():
+        conn2.send(body=con2_message, destination='/dune')
+        time.sleep(1)
+    for i, line in enumerate(dune_quote_split):
+        active_cons[i % 3].send(body=line, destination='/dune')
+        time.sleep(1)
+
+    time.sleep(2)
+    for con in active_cons:
+        if con.is_connected():
+            con.disconnect()
+    if conn2.is_connected():
+        print('conn 2 should not be connected and we disconnected it')
+        conn2.disconnect()
